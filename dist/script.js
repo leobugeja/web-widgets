a4baddff0ab554c0d8aa418cd453bed813ddcb2f
leobugeja <bugeja.leo@gmail.com>
2024-08-01 23:23:50 +0100

    Current week widget

---
+++ dist/script.js
@@ -0,0 +1,17 @@
+function getDateWeek(date) {
+  const januaryFirst = new Date(date.getFullYear(), 0, 1);
+  const daysToNextMonday =
+  januaryFirst.getDay() === 1 ? 0 :
+  (7 - januaryFirst.getDay()) % 7;
+  const nextMonday =
+  new Date(date.getFullYear(), 0,
+  januaryFirst.getDate() + daysToNextMonday);
+
+  return date < nextMonday ? 52 : date > nextMonday ? Math.ceil(
+  (date - nextMonday) / (24 * 3600 * 1000) / 7) : 1;
+}
+
+const today = new Date();
+
+const weekNumElement = document.getElementById('week-number');
+weekNumElement.innerText = getDateWeek(today);
