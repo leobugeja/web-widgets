fff58d37c9a934f4d49ecf724121bc2782c1b9f7
leobugeja <bugeja.leo@gmail.com>
2024-08-13 22:26:26 +0100

    Add upcoming plans text

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function getDateWeek(date) {
   const januaryFirst = new Date(date.getFullYear(), 0, 1);
-  const daysToNextMonday =
-  januaryFirst.getDay() === 1 ? 0 :
-  (7 - januaryFirst.getDay()) % 7;
-  const nextMonday =
-  new Date(date.getFullYear(), 0,
-  januaryFirst.getDate() + daysToNextMonday);
+  const daysToNextMonday = (januaryFirst.getDay() === 1) ? 0 : (7 - januaryFirst.getDay()) % 7;
+  const nextMonday = new Date(date.getFullYear(), 0, januaryFirst.getDate() + daysToNextMonday);
 
-  return date < nextMonday ? 52 : date > nextMonday ? Math.ceil(
-  (date - nextMonday) / (24 * 3600 * 1000) / 7) : 1;
+  return (date < nextMonday) ? 52 : (date > nextMonday ? Math.ceil((date - nextMonday) / (24 * 3600 * 1000) / 7) : 1);
 }
 
 const today = new Date();
+const nextWeek = new Date();
+nextWeek.setDate(nextWeek.getDate() + 7);
 
 const weekNumElement = document.getElementById('week-number');
 weekNumElement.innerText = getDateWeek(today);
+const nextWeekNumElement = document.getElementById('next-week-number');
+nextWeekNumElement.innerText = getDateWeek(nextWeek);
